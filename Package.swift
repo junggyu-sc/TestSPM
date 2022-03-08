a63d7ef78ada1459309414475edb07f0a5a913da
junggyu-sc <jung.gyu@safetyculture.io>
2022-03-09 08:24:48 +1100

    release

--- Package.swift
+++ Package.swift
@@ -9,8 +9,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "Crux",
-      url: "https://sc-mobile-packages.s3.amazonaws.com/crux/4.89.7/Crux.xcframework.zip",
-      checksum: "a85cf8bce1adadbd5cd85bec48464b3466e12fc289b549d0abe4f2321e88dad2"
+      url: "https://sc-mobile-packages.s3.amazonaws.com/crux/4.89.8/Crux.xcframework.zip",
+      checksum: "a85cf8bce1adadbd5cd85bec48464b3466e12fc289b549d0abe4f2321e88dad3"
     )
   ]
 )
